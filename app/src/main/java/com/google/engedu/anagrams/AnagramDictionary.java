38ff92f3df10666e84c93d9791bf67580efdc47d
harshitha97 <h.puranik25@gmail.com>
2016-10-06 20:57:09 +0530

    first commit

---
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -0,0 +1,54 @@
+package com.google.engedu.anagrams;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class AnagramDictionary {
+
+    private static final int MIN_NUM_ANAGRAMS = 5;
+    private static final int DEFAULT_WORD_LENGTH = 3;
+    private static final int MAX_WORD_LENGTH = 7;
+    private Random random = new Random();
+
+    private ArrayList<String> wordList;
+
+    public AnagramDictionary(InputStream wordListStream) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
+        wordList = new ArrayList<>();
+        String line;
+        while((line = in.readLine()) != null) {
+            String word = line.trim();
+            wordList.add(word);
+        }
+    }
+
+    private String sortLetters(String word) {
+        char[] alpha = word.toCharArray();
+        Arrays.sort(alpha);
+        String sorted = alpha.toString();
+        return sorted;
+    }
+
+    public boolean isGoodWord(String word, String base) {
+        return true;
+    }
+
+    public ArrayList<String> getAnagrams(String targetWord) {
+        ArrayList<String> result = new ArrayList<String>();
+        return result;
+    }
+
+    public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
+        ArrayList<String> result = new ArrayList<String>();
+        return result;
+    }
+
+    public String pickGoodStarterWord() {
+        return "stop";
+    }
+}
